53c0f20ffa31b9f46b4f9653a81b1872f11d3539
Harold Salaila <h.salaila@gmail.com>
2016-06-08 19:23:39 +0200

    Handle missing values

--- src/pandashandler.py
+++ src/pandashandler.py
@@ -12,6 +12,11 @@ def read_csv(file=DEFAULT_PATH):
 def filter_two_names(df, name1, name2):
     """filters the data frame on the specified names and groups it per year; regardless of gender.
     return dictionary key: <Name> value: pandas.DataFrame"""
+    min_index = df["Year"].min()
+    max_index = df["Year"].max() + 1
+
     def filter_name_groupby_year(name):
-        return df[df["Name"].isin([name])].groupby(["Year"])["Count"].sum()
+        return df[df["Name"].isin([name])].groupby(["Year"])["Count"]\
+            .sum()\
+            .reindex(range(min_index, max_index), fill_value=0)
     return {name1: filter_name_groupby_year(name1), name2: filter_name_groupby_year(name2)}
